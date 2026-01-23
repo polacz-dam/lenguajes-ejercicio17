fc9592c9ea7dd415372f6664859db08f57087ce6
Mohammed <mohahm7@alu.edu.gva.es>
2026-01-23 21:14:31 +0100

    commit1

---
+++ EJ17.js
@@ -0,0 +1,34 @@
+function CargarDatosEnTabla() {
+// Carga el archivo XML
+var request = new XMLHttpRequest();
+request.open("GET", "libros.xml", false);
+request.send();
+// Comprueba que se encontró el archivo
+if (request.status === 200) {
+var xmlDoc = request.responseXML;
+// Se obtienen todos los libros (elementos con la etiqueta <libro>) del archivo XML
+var libros = xmlDoc.getElementsByTagName("libro"); // Array
+// Se obtiene el elemento HTML que representa el cuerpo de la tabla
+var tableBody = document.getElementById("librosBody");
+// Se vacía el contenido de la tabla
+tableBody.innerHTML = "";
+// Se itera cada libro y se construye un nuevo elemento HTML para la fila de la tabla
+for (var i = 0; i < libros.length; i++) {
+var libro = libros[i];
+// Se crea una nueva fila en el HTML
+var row = tableBody.insertRow();
+// Se insertan celdas en la fila (HTML)
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+var cell3 = row.insertCell(2);
+var cell4 = row.insertCell(3);
+// Se cambia el contenido de cada celda con los datos del participante
+cell1.textContent = libro.getElementsByTagName("titulo")[0].textContent;
+cell2.textContent = libro.getElementsByTagName("autor")[0].textContent;
+cell3.textContent = libro.getElementsByTagName("anio")[0].textContent;
+cell4.textContent = libro.getElementsByTagName("precio")[0].textContent;
+}
+}
+}
+// llama a la función
+CargarDatosEnTabla();

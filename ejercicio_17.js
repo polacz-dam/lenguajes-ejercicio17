54a0ee3136ef3bd38498cfa3dd62b93627b38f25
Blanquiyu <bmonforte2@gmail.com>
2026-01-23 21:10:04 +0100

    ejercicio hecho

--- ejercicio_17.js
+++ ejercicio_17.js
@@ -1,40 +1,43 @@
 function CargarLibrosEnTabla() {
 
-var request = new XMLHttpRequest();
-request.open("GET", "libros.xml", false);
-request.send();
+    var request = new XMLHttpRequest();
+    request.open("GET", "libros.xml", false);
+    request.send();
 
-if (request.status === 200) {
-var xmlDoc = request.responseXML;
+    if (request.status === 200) {
+        var xmlDoc = request.responseXML;
 
-var buscador = document.getElementById("buscador");
+        var buscador = document.getElementById("buscador");
 
-var libros = xmlDoc.getElementsByTagName("libro"); 
+        var libros = xmlDoc.getElementsByTagName("libro");
 
-var tableBody = document.getElementById("libr");
+        var tableBody = document.getElementById("libr");
 
-tableBody.innerHTML = "";
+        tableBody.innerHTML = "";
 
-for (var i = 0; i < libros.length; i++) {
-var libro = libros[i];
+        for (var i = 0; i < libros.length; i++) {
+            var libro = libros[i];
+
+        if (buscador.value === "" || buscador.value == libro.getElementsByTagName("anio")[0].textContent) {
 
 
-var row = tableBody.insertRow();
+                var row = tableBody.insertRow();
 
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-var cell3 = row.insertCell(2);
-var cell4 = row.insertCell();
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                var cell4 = row.insertCell();
 
-cell1.textContent =
-libro.getElementsByTagName("titulo")[0].textContent;
-cell2.textContent =
-libro.getElementsByTagName("autor")[0].textContent;
-cell3.textContent =
-libro.getElementsByTagName("anio")[0].textContent;
-cell4.textContent =
-libro.getElementsByTagName("precio")[0].textContent;
-}
-}
+                cell1.textContent =
+                    libro.getElementsByTagName("titulo")[0].textContent;
+                cell2.textContent =
+                    libro.getElementsByTagName("autor")[0].textContent;
+                cell3.textContent =
+                    libro.getElementsByTagName("anio")[0].textContent;
+                cell4.textContent =
+                    libro.getElementsByTagName("precio")[0].textContent;
+            }
+        }
+    }
 }
 CargarLibrosEnTabla()

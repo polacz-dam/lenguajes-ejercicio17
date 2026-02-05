f10bbd9abe6e2fd287067149211691e694c31b9a
«Joan» <joadegsol@alu.edu.gva.es>
2026-02-05 17:27:28 +0100

    COMIT3

--- 17/biblioteca.js
+++ 17/biblioteca.js
@@ -1,7 +1,7 @@
 function CargarDatosEnTabla() {
     // Carga el archivo XML
     var request = new XMLHttpRequest();
-    request.open("GET", "libros.xml", false);
+    request.open("GET", "../libros.xml", false);
     request.send();
     // Comprueba que se encontró el archivo
     if (request.status === 200) {
